9d26574ea5a315cd60347a780e727e7a39f0f852
hirotaka121 <ajiayulong@gmail.com>
2022-09-09 11:57:32 +0900

    アップバー作成

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,14 @@ import Avatar from '@mui/material/Avatar';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
+import Grid from '@mui/material/Grid';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import InputBase from '@mui/material/InputBase';
+// import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
 
 const fetchData = async (keyword) => {
   const { API_HOST } = getConfig().publicRuntimeConfig;
@@ -41,40 +49,96 @@ const Shops = ({ firstViewShops }) => {
     setKeyword('');
   };
 
+  const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  }));
+
+  const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }));
+
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        width: '12ch',
+        '&:focus': {
+          width: '20ch',
+        },
+      },
+    },
+  }));
+
   return (
     <Container component="main" maxWidth="md">
-      <Box
-        component="form"
-        noValidate
-        maxWidth="md"
-        sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <TextField
-          label="キーワードを入力してください"
-          variant="standard"
-          margin="normal"
-          fullWidth
-          value={keyword}
-          onChange={(event) => {
-            setKeyword(event.target.value);
-          }}
-        />
-        <Button
-          variant="contained"
-          margin="normal"
-          fullWidth
-          onClick={() => {
-            onSearchClick();
-          }}
-        >
-          検索
-        </Button>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton size="large" edge="start" color="inherit" aria-label="open drawer" sx={{ mr: 2 }}>
+              {/* <MenuIcon /> */}
+            </IconButton>
+            <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}>
+              MUI
+            </Typography>
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase placeholder="Search…" inputProps={{ 'aria-label': 'search' }} />
+            </Search>
+          </Toolbar>
+        </AppBar>
       </Box>
+
+      <Grid container>
+        <Grid item xs={3}>
+          <TextField
+            label="キーワードを入力してください"
+            variant="standard"
+            margin="normal"
+            fullWidth
+            value={keyword}
+            onChange={(event) => {
+              setKeyword(event.target.value);
+            }}
+          />
+        </Grid>
+
+        <Grid item xs={3}>
+          <Button
+            variant="contained"
+            margin="normal"
+            Width="10px"
+            onClick={() => {
+              onSearchClick();
+            }}
+          >
+            検索
+          </Button>
+        </Grid>
+      </Grid>
       <Box
         component="form"
         noValidate
